270c88c428aea59cef8bf60e529790e01921a929
amarjeetprasad <56919207+amarjeetprasad@users.noreply.github.com>
2020-06-16 20:47:06 +0530

    Updates

---
+++ precache-manifest.570ddd8c8d58c3c7fdd77de2b47d3c42.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3040dbe998fd8ea9a8cd124dc69155fa",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "49cc1cec7eb0a830c950",
+    "url": "/robofriends/static/css/main.0d7466a7.chunk.css"
+  },
+  {
+    "revision": "09a2233482e68900cb97",
+    "url": "/robofriends/static/js/2.61207c98.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robofriends/static/js/2.61207c98.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "49cc1cec7eb0a830c950",
+    "url": "/robofriends/static/js/main.c44980f6.chunk.js"
+  },
+  {
+    "revision": "99557a09ab68caec6407",
+    "url": "/robofriends/static/js/runtime-main.229ccf3b.js"
+  }
+]);
